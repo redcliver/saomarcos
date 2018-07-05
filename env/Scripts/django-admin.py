e9c354e056bb497541386d25a0d4052d2e5f4ad7
Igor <igor-peres@hotmail.com>
2018-07-05 08:18:48 -0400

    Initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\headtec\source\repos\saomarcos\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ saomarcos/urls.py
@@ -0,0 +1,22 @@
+"""
+Definition of urls for saomarcos.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    url(r'^$', include('home.urls')),
+    # Examples:
+    # url(r'^$', saomarcos.views.home, name='home'),
+    # url(r'^saomarcos/', include('saomarcos.saomarcos.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
